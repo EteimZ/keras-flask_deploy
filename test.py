8c34bead0a333681358ee13ecd377dc92dd3489c
EteimZ <8teims@gmail.com>
2020-12-01 17:02:32 +0100

    Changed file path

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import requests
 
 # https://your-heroku-app-name.herokuapp.com/predict
 # http://localhost:5000/predict
-resp = requests.post("http://localhost:5000/predict", files={'file': open('/home/Eteims/repos/keras-flask-deploy/index.png', 'rb')})
+resp = requests.post("http://localhost:5000/predict", files={'file': open('img/eight.png', 'rb')})
 
 print(resp.text)
