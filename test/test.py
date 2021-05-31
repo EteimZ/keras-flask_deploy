c594d3ec95c82e0b71a90fc27281992f94f270bf
Eteimz <8teims@gmail.com>
2021-05-31 23:41:07 +0100

    Finishing touch

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
+#Test Script to make post request to the server
 import requests 
 
-# https://keras-flask.herokuapp.com/predict
 resp = requests.post("https://keras-flask.herokuapp.com/predict", files={'file': open('eight.png', 'rb')})
 
 print(resp.text)
